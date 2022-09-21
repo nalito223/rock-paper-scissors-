6a38fe46c71fe7c08012f2131a08190667265040
Tim T <tim.email.personal@gmail.com>
2022-09-21 13:12:17 -1000

    Add take turn functionality

--- src/main.js
+++ src/main.js
@@ -22,8 +22,12 @@ var classicPaper = document.querySelector('.classic-paper-button')
 var classicScissors = document.querySelector('.classic-scissors-button')
 var spicyLizard = document.querySelector('.spicy-lizard-button')
 var spicyAlien = document.querySelector('.spicy-alien-button')
+var spicyRock = document.querySelector('.spicy-rock-button')
+var spicyPaper = document.querySelector('.spicy-paper-button')
+var spicyScissors = document.querySelector('.spicy-scissors-button')
 // variables for get computer choice function 
 var classicOptionsArray = ["rock", "paper", "scissors"]
+var spicyOptionsArray = ["rock", "paper", "scissors", "alien", "lizard"]
 
 // var playerChoice = "paper"
 // var computerScore = 0
@@ -44,7 +48,14 @@ function goToStartView() {
   classicRock.classList.add('hidden')
   classicPaper.classList.add('hidden')
   classicScissors.classList.add('hidden')
+  // hide spicy optiosn
+  spicyPaper.classList.add('hidden')
+  spicyScissors.classList.add('hidden')
+  spicyLizard.classList.add('hidden')
+  spicyAlien.classList.add('hidden')
+  spicyRock.classList.add('hidden')
   classicButton.addEventListener('click', startClassicGame)
+  spicyButton.addEventListener('click', startSpicyGame)
 }
 
 function startClassicGame() {
@@ -62,6 +73,21 @@ function startClassicGame() {
   classicPaper.addEventListener('click', newGame.player.takeTurn)
   classicScissors.addEventListener('click', newGame.player.takeTurn)
 }
-// function getComputerChoice (
 
-// )
+function startSpicyGame() {
+  winnerText.innerText = ""
+  middleDialogueBox.innerText = "Choose your fighter!"
+  changeButton.classList.remove('hidden')
+  classicButton.classList.add('hidden')
+  spicyButton.classList.add('hidden')
+  spicyRock.classList.remove('hidden')
+  spicyPaper.classList.remove('hidden')
+  spicyScissors.classList.remove('hidden')
+  spicyAlien.classList.remove('hidden')
+  spicyLizard.classList.remove('hidden')
+  spicyRock.addEventListener('click', newGame.player.takeTurnSpicy)
+  spicyPaper.addEventListener('click', newGame.player.takeTurnSpicy)
+  spicyScissors.addEventListener('click', newGame.player.takeTurnSpicy)
+  spicyLizard.addEventListener('click', newGame.player.takeTurnSpicy)
+  spicyAlien.addEventListener('click', newGame.player.takeTurnSpicy)
+}
