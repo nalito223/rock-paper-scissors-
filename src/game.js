a1627386c3b2544180ccdfd31cbe9029ac5f75c5
Tim T <tim.email.personal@gmail.com>
2022-09-20 14:00:25 -1000

    Add initial html

---
+++ src/game.js
@@ -0,0 +1,10 @@
+class Game {
+  constructor() {
+    this.humanPlayer = new Player()
+    this.computerPlayer = new Player()
+    this.humanScore = 0
+    this.computerScore = 0
+    this.draw = draw
+
+  }
+}
---
+++ src/player.js
@@ -0,0 +1,8 @@
+class Player {
+  constructor(name, token, wins) {
+    this.name = name
+    this.token = token
+    this.wins = wins || 0
+  }
+  takeTurn()
+}
